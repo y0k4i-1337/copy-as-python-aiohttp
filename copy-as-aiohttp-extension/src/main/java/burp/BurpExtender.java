1818c09aa16d4c585937d0d8504af22e3abc099b
Mayk <7395296+y0k4i-1337@users.noreply.github.com>
2024-05-17 17:41:53 -0300

    Show messages on loading and unloading

--- copy-as-aiohttp-extension/src/main/java/burp/BurpExtender.java
+++ copy-as-aiohttp-extension/src/main/java/burp/BurpExtender.java
@@ -8,7 +8,9 @@ public class BurpExtender implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
         api.extension().setName("Copy as Python aiohttp");
-
         api.userInterface().registerContextMenuItemsProvider(new MyContextMenuItemsProvider(api));
+        api.extension().registerUnloadingHandler(
+                () -> api.logging().logToOutput("'Copy as Python aiohttp' extension unloaded!"));
+        api.logging().logToOutput("'Copy as Python aiohttp' extension loaded!");
     }
 }
